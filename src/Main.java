ae5959f6668d8d497cc383230fa681e3732bb755
iqbal <iqbal.official.ac@gmail.com>
2024-01-25 15:13:46 +0600

    tested with both circle and rectangle.. working fine

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         SingleTon Connection = SingleTon.getInstance();
         Shape c1 = Connection.getShape("CIRCLE");
         c1.draw();
+        Shape r1 = Connection.getShape("RECTANGLE");
+        r1.draw();
     }
 }
