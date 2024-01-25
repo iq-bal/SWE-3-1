9d8ad1bffbfc0295901f897092b176d8440d3311
iqbal <iqbal.official.ac@gmail.com>
2024-01-25 15:12:18 +0600

    inside main function .. working on instances of class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        SingleTon Connection = SingleTon.getInstance();
+        Shape c1 = Connection.getShape("CIRCLE");
+        c1.draw();
     }
 }
