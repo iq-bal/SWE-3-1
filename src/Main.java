90dcc9578f33bc1f1472c25e489f52879266c003
iqbal <iqbal.official.ac@gmail.com>
2024-01-25 15:24:08 +0600

    accepted

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,12 @@ public class Main {
     public static void main(String[] args) {
         SingleTon Connection = SingleTon.getInstance();
         Shape c1 = Connection.getShape("CIRCLE");
-        c1.draw();
-        Shape r1 = Connection.getShape("RECTANGLE");
-        r1.draw();
+//        c1.draw();
+        SingleTon NewConnection = SingleTon.getInstance();
+        Shape r1 = NewConnection.getShape("RECTANGLE");
+        System.out.println(Connection.hashCode());
+        System.out.println(NewConnection.hashCode());
+
+//        r1.draw();
     }
 }
