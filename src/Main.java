75197f1a5ae3cca7e88b350e9cd0b7525d26d49c
iqbal <iqbal.official.ac@gmail.com>
2024-02-02 18:35:09 +0600

    submission for assignment

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,212 @@
+interface TV{
+    boolean isEnabled();
+
+    void enable();
+
+    void disable();
+
+    int getVolume();
+
+    void setVolume(int vol);
+
+    int getChannel();
+
+    void setChannel(int channel);
+
+}
+
+class SmartTV implements TV{
+    boolean powerOn=false;
+    int volume=40;
+    int channel=1;
+
+    @Override
+    public boolean isEnabled() {
+        return powerOn;
+    }
+
+    @Override
+    public void enable() {
+        powerOn=true;
+    }
+
+    @Override
+    public void disable() {
+        powerOn=false;
+    }
+
+    @Override
+    public int getVolume() {
+        return volume;
+    }
+
+    @Override
+    public void setVolume(int vol) {
+        volume=vol;
+    }
+
+    @Override
+    public int getChannel() {
+        return channel;
+    }
+
+    @Override
+    public void setChannel(int channel) {
+        this.channel=channel;
+    }
+    public void Youtube(YoutubeClass yt){
+        System.out.println("Youtube for SmartTV::");
+        yt.runYoutube();
+    }
+}
+
+class GeneralTV implements TV{
+    boolean powerOn=false;
+    int volume=20;
+    int channel=1;
+
+    @Override
+    public boolean isEnabled() {
+        return powerOn;
+    }
+
+    @Override
+    public void enable() {
+        powerOn=true;
+    }
+
+    @Override
+    public void disable() {
+        powerOn=false;
+    }
+
+    @Override
+    public int getVolume() {
+        return volume;
+    }
+
+    @Override
+    public void setVolume(int vol) {
+        volume=vol;
+    }
+
+    @Override
+    public int getChannel() {
+        return channel;
+    }
+
+    @Override
+    public void setChannel(int channel) {
+        this.channel=channel;
+    }
+}
+
+
+class Remote{
+    protected TV tv;
+
+    Remote(){tv=null;}
+    Remote(TV tv){
+        this.tv=tv;
+    }
+    public void togglePower() {
+        if(tv.isEnabled()){
+            tv.disable();
+            System.out.println("Power turned off");
+        }
+        else {
+            tv.enable();
+            System.out.println("Power turned on");
+        }
+    }
+
+    public void volumeUp() {
+        if(tv.isEnabled()){
+            tv.setVolume(tv.getVolume()+5);
+            System.out.println("Increasing volume +5");
+        }else{
+            System.out.println("Please Turn on tv first");
+        }
+    }
+
+    public void volumeDown() {
+        if(tv.isEnabled()){
+            tv.setVolume(tv.getVolume()-5);
+            System.out.println("Decreasing volume -5");
+        }else{
+            System.out.println("Please Turn on tv first");
+        }
+
+    }
+
+    public void channelUp() {
+        if(tv.isEnabled()){
+            tv.setChannel(tv.getVolume()+1);
+            System.out.println("Channel +1");
+        }else{
+            System.out.println("Please Turn on tv first");
+        }
+
+
+    }
+
+    public void channelDown() {
+        if(tv.isEnabled()){
+            tv.setChannel(tv.getVolume()-1);
+            System.out.println("Channel -1");
+        }else{
+            System.out.println("Please Turn on tv first");
+        }
+
+    }
+}
+
+class SmartRemote extends Remote{
+
+    SmartRemote(){
+
+    }
+    SmartRemote(TV tv){
+        super(tv);
+    }
+    void showYoutube(YoutubeClass yt){
+        ((SmartTV)tv).Youtube(yt);
+    }
+}
+
+
+interface YoutubeClass{
+    void runYoutube();
+}
+class FirstLoadYoutube implements YoutubeClass{
+
+    @Override
+    public void runYoutube() {
+        System.out.println("Running Youtube");
+    }
+}
+
+class proxyLoadYoutube implements YoutubeClass{
+    private FirstLoadYoutube firstLoadYoutube;
+    @Override
+    public void runYoutube() {
+        if(firstLoadYoutube==null){
+            firstLoadYoutube=new FirstLoadYoutube();
+            System.out.println("Starting Youtube");
+        }
+        firstLoadYoutube.runYoutube();
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        SingleTon Connection = SingleTon.getInstance();
-        Shape c1 = Connection.getShape("CIRCLE");
-//        c1.draw();
-        SingleTon NewConnection = SingleTon.getInstance();
-        Shape r1 = NewConnection.getShape("RECTANGLE");
-        System.out.println(Connection.hashCode());
-        System.out.println(NewConnection.hashCode());
 
-//        r1.draw();
+        GeneralTV gtv=new GeneralTV();
+        Remote remote= new Remote(gtv);
+        SmartTV smarttv=new SmartTV();
+        SmartRemote smartremote= new SmartRemote(smarttv);
+        YoutubeClass yt=new proxyLoadYoutube();
+        smartremote.showYoutube(yt);
+        smartremote.showYoutube(yt);
     }
 }
